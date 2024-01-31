b5486d962437ba8a54528f0fcaddb71c156f12cc
ecsimsw <ecsimsw@gmail.com>
2024-02-01 05:54:18 +0900

    rename

--- src/main/java/ecsimsw/ratelimit/LeakyBucket.java
+++ src/main/java/ecsimsw/ratelimit/LeakyBucket.java
@@ -17,14 +17,7 @@ public class LeakyBucket<T> {
         this.flowRate = flowRate;
         this.capacity = capacity;
         this.waitings = new ArrayBlockingQueue<>(capacity);
-
-        var scheduleService = Executors.newScheduledThreadPool(1);
-        scheduleService.scheduleAtFixedRate(() -> {
-            if (!waitings.isEmpty()) {
-                waitings.poll();
-                LOGGER.info("release, waitings : " + waitings.size());
-            }
-        }, 0, flowRate, TimeUnit.MILLISECONDS);
+        fixedFlow(flowRate);
     }
 
     public void put(T id) {
@@ -50,5 +43,15 @@ public class LeakyBucket<T> {
             }
         }
         throw new TimeoutException("time out");
+    }
+
+    private void fixedFlow(int flowRate) {
+        var scheduleService = Executors.newScheduledThreadPool(1);
+        scheduleService.scheduleAtFixedRate(() -> {
+            if (!waitings.isEmpty()) {
+                waitings.poll();
+                LOGGER.info("release, waitings : " + waitings.size());
+            }
+        }, 0, flowRate, TimeUnit.MILLISECONDS);
     }
 }
