b3efe3719272fccd7f9d12d40aac8d134e48ce18
ecsimsw <ecsimsw@gmail.com>
2024-02-11 20:09:42 +0900

    test RateLimitCounter

--- src/main/java/ecsimsw/usage/MainController.java
+++ src/main/java/ecsimsw/usage/MainController.java
@@ -1,9 +1,12 @@
 package ecsimsw.usage;
 
 import ecsimsw.ratelimit.RateLimit;
+import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
 
 @RestController
 public class MainController {
--- src/test/java/ecsimsw/ratelimit/GuavaRateLimitTest.java
+++ src/test/java/ecsimsw/ratelimit/GuavaRateLimitTest.java
@@ -1,27 +1,26 @@
 package ecsimsw.ratelimit;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("RateLimitCounter 테스트")
-public class RateLimitCounterTest {
+@DisplayName("Guava rateLimiter 테스트")
+public class GuavaRateLimitTest {
 
     @Test
-    public void testRateLimitCounter() {
+    public void testGuavaRateLimiter() {
         // Limit request per 0.1 sec
-        var rate = 100;
+        var ratePerSec = 0.1;
         var queueingCount = 5;
 
-        var rateLimiter = new RateLimitCounter(100, rate, false);
-
+        var rateLimiter = RateLimiter.create(1/ratePerSec);
         var startTime = System.currentTimeMillis();
         for(int i =0; i<queueingCount; i++) {
-            rateLimiter.count();
+            rateLimiter.acquire(1);
         }
         var endTime = System.currentTimeMillis();
-
-        assertThat(endTime - startTime).isGreaterThanOrEqualTo(rate * (queueingCount-1));
+        assertThat(endTime - startTime).isGreaterThanOrEqualTo((long) (ratePerSec * (queueingCount-1) * 1000));
     }
 }
