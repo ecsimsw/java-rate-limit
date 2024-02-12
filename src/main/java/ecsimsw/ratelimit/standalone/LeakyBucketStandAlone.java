a50ebd849dcde9d4ecc20406e46edb1c615085af
ecsimsw <ecsimsw@gmail.com>
2024-02-13 03:33:51 +0900

    Redis concurrent test

---
+++ src/main/java/ecsimsw/ratelimit/standalone/LeakyBucketStandAlone.java
@@ -0,0 +1,59 @@
+package ecsimsw.ratelimit.standalone;
+
+import ecsimsw.ratelimit.BucketFullException;
+import ecsimsw.ratelimit.LeakyBucket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.*;
+
+public class LeakyBucketStandAlone<T> implements LeakyBucket<T> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LeakyBucketStandAlone.class);
+
+    private final int flowRate;
+    private final int capacity;
+    private final BlockingQueue<T> waitings;
+
+    public LeakyBucketStandAlone(int flowRate, int capacity) {
+        this.flowRate = flowRate;
+        this.capacity = capacity;
+        this.waitings = new ArrayBlockingQueue<>(capacity);
+        fixedFlow(flowRate);
+    }
+
+    public void put(T id) {
+        try {
+            waitings.add(id);
+            LOGGER.info("block, waitings : " + waitings.size());
+        } catch (IllegalStateException e) {
+            throw new BucketFullException("bucket full");
+        }
+    }
+
+    public void putAndWait(T id) throws TimeoutException {
+        put(id);
+        var startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < (long) flowRate * capacity) {
+            if (!waitings.contains(id)) {
+                return;
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        throw new TimeoutException("time out");
+    }
+
+    private void fixedFlow(int flowRate) {
+        var scheduleService = Executors.newScheduledThreadPool(1);
+        scheduleService.scheduleAtFixedRate(() -> {
+            if (!waitings.isEmpty()) {
+                waitings.poll();
+                LOGGER.info("release, waitings : " + waitings.size());
+            }
+        }, 0, flowRate, TimeUnit.MILLISECONDS);
+    }
+}
